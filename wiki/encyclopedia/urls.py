c8c476d062ea3be6449816f4fcc616f3af557e41
Renato Luiz <renatoluiz9834@hotmail.com>
2023-07-31 18:49:26 -0300

    busca

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("search/", views.search, name="search"),
-    path("newpage",views.create_newpage,name="create_newpage")
+    path('wiki/<str:title>/', views.search, name='search'),
+    path("search_title/<str:title>/", views.search_title, name="search_title"),
+    path("newpage/",views.create_newpage,name="create_newpage"),
+    path("search_random/",views.search_random,name="search_random"),
+    path("notFound/", views.not_found, name="not_found"),
+    path("editpage/",views.edit_page, name="edit_page"),
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,28 +1,90 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.utils.safestring import mark_safe
+from django.contrib import messages
+from django.urls import reverse
 import markdown
+import random
 
 from . import util
-
-
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
-def search(request):
-    result_search = markdown.markdown(util.get_entry(request.GET.get("q")))
+
+def search(request, title=""):
+    title = title
+    if not title:
+        return redirect('index')
+    
+    entries = util.list_entries()
+
+    search_title = title in entries
+    
+    if not search_title or search_title is None:
+        return redirect(not_found)
+
+    
+    result_search = markdown.markdown(util.get_entry(title))
     result_search = mark_safe(result_search)
+    
     return render(request, 'encyclopedia/search.html', {
-        "entry": result_search 
-    })
+                "entry": result_search,
+                "title": title,
+        })
+
+def search_title(request, title):
+    title =  request.GET.get('title',"")
+    if not title:
+        return redirect('index')
+    return redirect('search', title = title)
+    
+
+
+def not_found(request):
+     title = "Not Found!"
+     text = "Sorry, not found !!!"
+     return render(request, 'encyclopedia/not_found.html', {
+                "entry": text,
+                "title": title,
+        })
 
 def create_newpage(request):
-    if request.method == 'POST':
+    list_title = util.list_entries()
+    title = request.POST.get('title',"")
+    result = title in list_title
+    if request.method == 'POST' and result == False and title != "":
         title = request.POST.get('title',"")
         text = request.POST.get('text',"")
+        messages.success(request, 'New page successfully created!')
         return render(request, 'encyclopedia/create_newpage.html', {
             'create_newpage': util.save_entry(title,text)
         })
-    return render(request,'encyclopedia/create_newpage.html',)
+    else:
+        if result == True:
+            error = "Error, invalid title!"
+            return render(request, 'encyclopedia/create_newpage.html', {'error_input': error})
+    
+    
+    return render(request,'encyclopedia/create_newpage.html')
+
+
+def search_random(request):
+    title = random.choice(util.list_entries())
+    return redirect('search', title = title)
+
+def edit_page(request):
+   if request.method == 'GET':
+        title = request.POST.get('title',"")
+        text = request.POST.get('text',"")
+        messages.success(request, 'New page successfully created!')
+        util.save_entry(title,text)
+        return redirect('search', title=title)
+   else:
+        title = request.GET.get('title', '')
+        result_search = util.get_entry(title)
+        return render(request, 'encyclopedia/edit_page.html', {
+                        "title_editvalue": title,
+                        "text_editvalue": result_search,
+                        })
